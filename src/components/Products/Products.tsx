8963cb344803d3ae6ebd90e608bebf3c0137b07b
youmekko <youmekko.o@gmail.com>
2020-07-30 15:23:27 +0900

    feat :  Products state 추가 및 Card 컴포넌트 구현

--- src/components/Products/Products.tsx
+++ src/components/Products/Products.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { Product } from '../types/types'
 
-function Products() {
+function Card(props) {
     return (
         <div>
-            Products
+            <div>
+                <img src={props.product.coverImage} alt={props.product.title} />
+            </div>
+            <div>
+                {props.product.title}
+            </div>
+            <div>
+                 {props.product.price}
+            </div>
+            <div>
+                Add to Cart
+            </div>
+        </div>
+    )
+}
+
+function Products() {
+
+    const [Products, setProducts] = useState([])
+
+    useEffect(() => {
+        fetch('http://localhost:3001/data/productItems.json')
+            .then(res => res.json())
+            .then(res => {
+                if (res.data) {
+                    setProducts(res.data)
+                } else {
+                    alert('Failed')
+                }
+               
+            })
+    }, [])
+
+    const renderCard = Products.map((product:Product, idx) => {
+        return <Card product={product} key={idx} />
+    })
+    
+    return (
+        <div>
+            <div>
+                <h2>Class101</h2>
+            </div>
+            <div>
+                {renderCard}
+            </div>
         </div>
     )
 }
