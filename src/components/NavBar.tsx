60bce30f8dcd333e066142a10e132513343f308b
youmekko <youmekko.o@gmail.com>
2020-08-06 22:53:01 +0900

    feat : 장바구니에 담긴 상품 개수 표시하는 UI 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,16 +1,26 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-function NavBar() {
+function NavBar({ quantity }) {
     return (
         <div>
             <div>
                 <h1>CLASS101</h1>
             </div>
             <div>
-                장바구니
+                장바구니[{quantity}]
             </div>
         </div>
     )
 }
 
-export default NavBar
+
+const mapStateToProps = state => {
+    const { cart } = state
+    const quantity = cart.itemList.length
+    return { quantity }
+}
+
+export default connect(mapStateToProps)(NavBar)
+
+
