1798e32ab92bb2ae12543e1480d12f3ae273808e
youmekko <youmekko.o@gmail.com>
2020-08-07 16:58:49 +0900

    feat : Cart 컴포넌트 구현 중

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -1,11 +1,54 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { Product } from '../types/types'
 
-function Cart() {
+function Cart({ itemList }) {
     return (
         <div>
-            Cart
+            <table>
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" /></th>
+                        <th colSpan={2}>클래스명</th>
+                        <th>수량</th>
+                        <th>가격</th>
+                        <th>쿠폰 적용</th>
+                    </tr>
+                </thead>
+                
+                 {itemList.length ? 
+                    (<tbody>
+                        {itemList.map((item:Product, idx) => {
+                            return (
+                            <tr key={idx}>
+                                <td><input type="checkbox" /></td>
+                                <td><img src={item.coverImage} style={{ width: '200px' }}/></td>
+                                <td>{item.title}</td>
+                                <td>{item.quantity}</td>
+                                <td>{item.price}</td>
+                                <td>{item.availableCoupon === undefined ? '가능' : '불가'}</td>
+                            </tr>
+                            )
+                        })}
+                         </tbody>
+                    )
+                    : 
+                    (<tbody>
+                        <tr>
+                            <td colSpan={6}>장바구니에 담긴 상품이 없습니다.</td>
+                        </tr>
+                    </tbody>)
+                }
+               
+            </table>
         </div>
     )
 }
 
-export default Cart
+const mapStateToProps = state => {
+    const { cart } = state
+    const { itemList } = cart
+    return { itemList }
+}
+
+export default connect(mapStateToProps)(Cart)
